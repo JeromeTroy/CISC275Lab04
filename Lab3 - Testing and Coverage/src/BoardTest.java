7b0c257c4acf6be6d6c5548549a47ec5714f3c0e
Jerome Troy <jrtroy@udel.edu>
2017-09-25 21:56:09 -0400

    We are finished

--- Lab3 - Testing and Coverage/src/BoardTest.java
+++ Lab3 - Testing and Coverage/src/BoardTest.java
@@ -14,6 +14,7 @@ public class BoardTest {
 		assertTrue("Timer is 150", b.timer==150);
 		assertTrue("Difficulty is 3", c.difficulty==1);
 		assertTrue("Timer is 150", c.timer==150);
+		assertTrue("Board is empty", b.Rocks.isEmpty());
 		
 		
 	}
@@ -21,25 +22,38 @@ public class BoardTest {
 	@Test
 	public void testRockSpawn(){
 		Board b = new Board(3);
-		//b.spawnRock();
+		b.spawnRock();
+		assertFalse("Board's list of rocks is not empty", b.Rocks.isEmpty());
+		assertTrue("Board's list of rocks has 1 rock", b.Rocks.size()==1);
 	}
 	
 	@Test 
 	public void testTimer(){
 		Board b = new Board(3);
+		Board v = new Board(1);
 		b.setTimer(740);
-		assertTrue("Timer is 740", b.timer==740);
-		assertFalse("Timer is not 150", b.timer==150);
+		v.setTimer(200);
+		assertTrue("B's timer is 740", b.timer==740);
+		assertFalse("B's timer is not 150", b.timer==150);
+		assertFalse("Timers are not equal", b.timer==v.timer);
 	}
 	
 	@Test
 	public void testMoveRocks(){
+		Board b = new Board(3);
+		b.spawnRock();
+		assertTrue("Rock is at 0", b.Rocks.get(0).getY()==0);
+		b.moveRocks();
+		//assertFalse("Rock has moved", b.Rocks.get(0).getY()==0);
 		
 	}
 	
 	@Test 
 	public void testRemoveRock(){
-		
+		Board b = new Board(3);
+		b.spawnRock();
+		b.removeRock(0);
+		assertTrue("Board is emptied", b.Rocks.isEmpty());
 	}
 	
 	@Test
@@ -51,5 +65,29 @@ public class BoardTest {
 	public void testMoveRight(){
 		
 	}
+	@Test
+	public void testScoring(){
+		Board b = new Board(3);
+		b.scoring(3, b.timer);
+		int sc = b.player.getScore();
+		assertTrue("Score is zero", b.player.getScore()==0);
+	}
+	@Test
+	public void testTakeTurn(){
+		Board b = new Board(3);
+		//b.takeTurn(1);
+		//assertFalse("Board is non-empty", b.Rocks.isEmpty());
+		
+		
+	}
+	@Test
+	public void testProcessChar(){
+		Board b = new Board(3);
+		b.processChar(3);
+		//assertTrue("Char is k", b.player.getScore()!=0);
+		
+		
+	}
+
 
 }
